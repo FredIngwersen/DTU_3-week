584c1ca7b6a09c240af111e94b8c9399d247aa60
William BenEmbarek <william.benembarek@gmail.com>
2017-01-05 12:16:10 +0100

    Working on game logic

---
+++ IRC_Server/gameClass.java
@@ -0,0 +1,61 @@
+package IRC_Server;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by William Ben Embarek on 05/01/2017.
+ */
+public class gameClass {
+	int totalp1;
+	int totalp2;
+
+	public int rollDice() //Rolls 2 dice and returns an int of the 2 dice (roll 3 and 6, returns 63)
+	{
+		String total;
+		int dice1 = ThreadLocalRandom.current().nextInt(1, 7);
+		int dice2 = ThreadLocalRandom.current().nextInt(1, 7);
+		if (dice1 > dice2) {
+			total = Integer.toString(dice1) + Integer.toString(dice2);
+		} else if (dice2 > dice1) {
+			total = Integer.toString(dice2) + Integer.toString(dice1);
+		} else {
+			total = Integer.toString(dice1) + Integer.toString(dice2);
+		}
+		return Integer.parseInt(total);
+	}
+
+	public boolean beatRoll(int totalp1, int totalp2) { //Returns a true if p1 beat p2's roll, else returns false
+		boolean p1Doubles = false;
+		boolean p2Doubles = false;
+		if (Math.floor(totalp1 / 10) == totalp1 % Math.floor(totalp1 / 10) * 10) {
+			p1Doubles = true;
+		}
+		if (Math.floor(totalp2 / 10) == totalp2 % Math.floor(totalp2 / 10) * 10) {
+			p2Doubles = true;
+		}
+		if (totalp1 == 21 && totalp2 != 21) {
+			return true;
+		}
+		else if (totalp1 == 31 && (totalp2 != 21 && totalp2 != 31)) {
+			return true;
+		}
+		else if (p1Doubles && p2Doubles) {
+			if (Math.floor(totalp1 / 10) > Math.floor(totalp2 / 10)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		else if (p1Doubles && !(p2Doubles || totalp2 == 21 || totalp2 == 31)){
+			return true;
+		}
+		else if (totalp1 > totalp2 && !((p2Doubles || totalp2 == 21 || totalp2 == 31))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	
+}
