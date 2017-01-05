1d42f3846948370897ce4465f62202760a079e8e
William BenEmbarek <william.benembarek@gmail.com>
2017-01-05 10:43:39 +0100

    Message Queue System added / implemented

--- IRC_Server/WorkerRunnable.java
+++ IRC_Server/WorkerRunnable.java
@@ -1,8 +1,6 @@
 package IRC_Server;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 
@@ -10,7 +8,6 @@ public class WorkerRunnable implements Runnable{
 
 	protected Socket clientSocket = null;
 	protected String serverText   = null;
-
 	public WorkerRunnable(Socket clientSocket, String serverText) {
 		this.clientSocket = clientSocket;
 		this.serverText   = serverText;
@@ -19,12 +16,14 @@ public class WorkerRunnable implements Runnable{
 	public void run() {
 		try {
 			InputStream input  = clientSocket.getInputStream();
+			BufferedReader BIS = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			OutputStream output = clientSocket.getOutputStream();
 			long time = System.currentTimeMillis();
 			output.write(("HTTP/1.1 200 OK\n\nWorkerRunnable: " +
 					this.serverText + " - " +
 					time +
 					"").getBytes());
+
 			output.close();
 			input.close();
 			System.out.println("Request processed: " + time);
@@ -33,4 +32,5 @@ public class WorkerRunnable implements Runnable{
 			e.printStackTrace();
 		}
 	}
+
 }
